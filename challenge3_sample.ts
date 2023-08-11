b7340807f8c5a2ce803fa8c9c8d19f9b7f50eda1
DetectiveJeremy <jeremy.r.herrera.ortiz@gmail.com>
2023-08-10 23:34:23 -0400

    Wrote challenge 3 in the challenge3_sample.ts and fixed the errors in challenge1.ts

--- challenge3_sample.ts
+++ challenge3_sample.ts
@@ -5,14 +5,36 @@ interface Car {
     manufacturer: string;
     year: number;
     color: string;
-    inWarranty: boolean
+    inWarranty: boolean;
     miles?: number;
 }
 
-const corolla: Car = {
+const Corolla: Car = {
     model: "Corolla",
     manufacturer: "Toyota",
     year: 2006,
     color: "Red",
     inWarranty: true
 }
+
+console.log(Corolla)
+
+interface Computer {
+gpu?:string | Array<string>;
+cpu:string;
+OS:string;
+SSD?: boolean;
+HDD?:boolean;
+PSU: string | Array<string>
+};
+
+const someonesComputer: Computer = {
+gpu:"3070 trinity TI",
+cpu: "i5 10000 series",
+OS: "Windows",
+SSD: true,
+HDD: true,
+PSU: "Corsair 750"
+};
+
+console.log(someonesComputer)
